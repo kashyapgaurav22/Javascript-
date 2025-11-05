6910ffa53316a35e8ffe918ab1e4e5de74bfce39
Gaurav raj <153973339+kashyapgaurav22@users.noreply.github.com>
2025-11-05 11:58:34 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,26 @@
+let value=null
+
+let valueIntoNumber=Number(value)
+
+console.log(typeof value)
+
+console.log(typeof valueIntoNumber)
+
+console.log(valueIntoNumber)
+
+// "33" => number
+// "33abc" = NaN
+// ture =>1 , false=>0 
+//undefined => NaN
+//null => 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1=>true 0=>false
+// ""=>false "gaurav" => true 
+
+
