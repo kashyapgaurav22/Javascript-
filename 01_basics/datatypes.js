b074193079caaa4f04ffcfc3cb919336b580647d
Gaurav raj <153973339+kashyapgaurav22@users.noreply.github.com>
2025-11-05 08:48:28 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code to new version 
+
+// alert(3+3) //bcoz we're using nodejs
+
+let name="gaurav"
+let age=19
+let isLoggedIn=true 
+
+//number =>2 to power 53
+//bigint
+//string = ""
+//boolean= ture/false
+//null => standalone value
+//symbol => unique
+
+//object 
+
+console.log(typeof age)
+
+console.log(typeof null) // it shows object ( language issue)
+
+console.log(typeof undefined) ///undefined
