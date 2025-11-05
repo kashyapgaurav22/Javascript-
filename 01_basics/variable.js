94292607c06a427a5f1a8d9e975e37c791c5588c
Gaurav raj <153973339+kashyapgaurav22@users.noreply.github.com>
2025-11-05 07:39:50 +0000

    learned about variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId=56756
+let emailAddress="kashya.gaurav0407@gmail.com"
+var password="gaurav@123"
+
+// accountId=456
+console.log(accountId)
+
+emailAddress="nikitaverma12222006@gmil.com"
+password="nikita@123"
+
+/*
+prefer not to use var ( issue in block scope and functional scope)
+*/
+
+console.table([accountId,emailAddress,password])
