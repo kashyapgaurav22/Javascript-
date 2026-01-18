5d0aa7574e878a7dbd265f0bc3174208263ef9a5
Gaurav raj <153973339+kashyapgaurav22@users.noreply.github.com>
2026-01-18 10:18:19 +0000

    comparison.js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,12 @@
+//console.log(2>1)
+//console.log("2">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(null!=0)
+
+// it's becuase comparsion <,>,<=,>= and equality == operater
+//works differently , null>0 => null is not a number , null>=0 , > coverts it to 0 and then null==0 , gives true
+
+console.log("2"===2) // it checks for data type as well 
