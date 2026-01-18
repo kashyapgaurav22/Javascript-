8d386305b65b1109d59cc73c632206aa4b23c618
Gaurav raj <153973339+kashyapgaurav22@users.noreply.github.com>
2026-01-18 10:59:52 +0000

    summary of data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+//primitive
+
+// 7 types : string , number , boolean , null , BigInt , undefined , symbol 
+
+//reference(Non-primitive)
+
+//arrays , objects , functions
+
+const bignumber=467543863
+const temp=null
+
+const nikita = ["pookie", "cutie","honey"];
+let myobj= {
+    name:"gaurav",
+    age:19,
+}
+
+const MyFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bignumber) // to know the type of our variable
+console.log(typeof temp) // type of null is object 
+
+//curly bracket ke andar ki chije objects hoti hai 
