0a29256b1d6e5bd917805887d11f671522bfabc1
Gaurav raj <153973339+kashyapgaurav22@users.noreply.github.com>
2026-01-19 16:38:56 +0000

    update

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -23,3 +23,26 @@ console.log(typeof bignumber) // to know the type of our variable
 console.log(typeof temp) // type of null is object 
 
 //curly bracket ke andar ki chije objects hoti hai 
+
+//stack => primitive , heap => non-primitive
+
+let ab ="hello"
+let gf=ab
+gf="pookie"
+console.log(ab)
+console.log(gf)
+
+let firstone={
+    email: "kashyap.gaurv0407@gmil.com",
+    number: 9199479587
+}
+
+let secondone=firstone
+
+secondone.email="nikitaverma22@gmail.com"
+secondone.number="8144678980"
+
+console.log(firstone.email)
+console.log(firstone.number)
+
+
